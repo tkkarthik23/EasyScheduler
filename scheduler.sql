a1eb46549f6ac5b5ffa3c8e546f569c2ee7b7438
Karthik Thokala <tkkarthik23@gmail.com>
2024-01-15 05:48:42 -0700

    Updating empty DB file

--- scheduler.sql
+++ scheduler.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 12, 2024 at 04:21 AM
+-- Generation Time: Jan 15, 2024 at 01:47 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.3.33
 
@@ -35,18 +35,6 @@ CREATE TABLE `assinedtable` (
   `projectedDate` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `assinedtable`
---
-
-INSERT INTO `assinedtable` (`id`, `taskId`, `userId`, `sprintId`, `projectedDate`) VALUES
-(1, 15, 5, 3, '2024-02-03'),
-(2, 20, 6, 3, '2024-02-07'),
-(3, 19, 6, 3, '2024-02-10'),
-(4, 14, 5, 3, '2024-02-06'),
-(5, 18, 5, 3, '2024-02-09'),
-(6, 16, 6, 3, '2024-02-12');
-
 -- --------------------------------------------------------
 
 --
@@ -58,18 +46,9 @@ CREATE TABLE `sprints` (
   `sprintName` varchar(225) NOT NULL,
   `startDate` date NOT NULL,
   `endDate` date NOT NULL,
-  `sprintId` int(225) NOT NULL,
+  `sprintId` int(225) DEFAULT NULL,
   `status` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `sprints`
---
-
-INSERT INTO `sprints` (`id`, `sprintName`, `startDate`, `endDate`, `sprintId`, `status`) VALUES
-(1, 'Dummy sprint', '2024-01-18', '2024-02-01', 0, 'notPlanned'),
-(2, 'New Sprint', '2024-01-25', '2024-02-08', 0, 'notPlanned'),
-(3, 'Thursday', '2024-01-31', '2024-02-14', 0, 'Planned');
 
 -- --------------------------------------------------------
 
@@ -87,29 +66,6 @@ CREATE TABLE `tasks` (
   `sprintId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `tasks`
---
-
-INSERT INTO `tasks` (`sno`, `taskName`, `taskDesc`, `priority`, `needBy`, `points`, `sprintId`) VALUES
-(2, 'Task 1', 'desc', 3, '2024-02-01', 2, 1),
-(3, 'Task 2', 'desc', 3, '2024-02-01', 3, 1),
-(4, 'Task 3', 'desc', 2, '2024-02-01', 3, 1),
-(5, 'Task 4', 'Task 5', 1, '2024-02-01', 7, 1),
-(6, 'Task 9', 'Desc', 2, '2024-02-01', 3, 1),
-(7, 'Task 88', 'desc', 1, '2024-02-01', 2, 1),
-(8, 'Task 1', 'Desc', 2, '2024-01-31', 7, 2),
-(9, 'Task 2', 'Desc', 3, '2024-02-02', 2, 2),
-(11, 'Task 3', 'Desc', 1, '2024-02-06', 3, 2),
-(12, 'Task 4', 'Desc', 1, '2024-02-06', 7, 2),
-(13, 'Task 6', 'desc', 3, '2024-02-06', 1, 2),
-(14, 'T1', 'desc', 2, '2024-02-08', 3, 3),
-(15, 'T2', 'desc', 3, '2024-02-05', 3, 3),
-(16, 'T3', 'Desc', 1, '2024-02-14', 2, 3),
-(18, 'T5', 'DESC', 2, '2024-02-09', 3, 3),
-(19, 't4', 'desc', 3, '2024-02-14', 3, 3),
-(20, 'T8', 'DESC', 3, '2024-02-06', 7, 3);
-
 -- --------------------------------------------------------
 
 --
@@ -122,18 +78,6 @@ CREATE TABLE `users` (
   `userEmail` varchar(225) NOT NULL,
   `sprintId` int(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `userName`, `userEmail`, `sprintId`) VALUES
-(1, 'user 1', 'tkkarthik23@gmail.com', 1),
-(2, 'User 2', 'kthokala@syr.edu', 1),
-(3, 'Karthik', 'karthikthokala6295@gmail.com', 2),
-(4, 'Azhar', 'azharDummy@example.com', 2),
-(5, 'Thokala', 'karthikthokala6295@gmail.com', 3),
-(6, 'Azhar', 'az@exaple.com', 3);
 
 --
 -- Indexes for dumped tables
@@ -171,25 +115,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `assinedtable`
 --
 ALTER TABLE `assinedtable`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `sprints`
 --
 ALTER TABLE `sprints`
-  MODIFY `id` int(225) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(225) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `sno` int(225) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `sno` int(225) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(225) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(225) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
